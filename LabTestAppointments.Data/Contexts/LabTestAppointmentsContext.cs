f5a62a736716fc977bf12bdd8ec34b966060965f
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-06-27 13:53:23 -0400

    Created IGenericRepository interface.

--- LabTestAppointments.Data/Contexts/LabTestAppointmentsContext.cs
+++ LabTestAppointments.Data/Contexts/LabTestAppointmentsContext.cs
@@ -7,7 +7,7 @@ namespace LabTestAppointments.Data.Contexts
     {
         public LabTestAppointmentsContext(DbContextOptions<LabTestAppointmentsContext> options) : base(options)
         {
-
+            
         }
     }
 }
