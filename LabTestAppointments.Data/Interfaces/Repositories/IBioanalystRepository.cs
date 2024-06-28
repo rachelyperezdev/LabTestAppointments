5b7026c380e232ebf93089bac661c6fd8a2cf492
HeyHector5 <hectorgmedina05@gmail.com>
2024-06-27 21:25:36 -0400

    Adding Bioanalyst interface.

---
+++ LabTestAppointments.Data/Interfaces/Repositories/IBioanalystRepository.cs
@@ -0,0 +1,9 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories.Generic;
+
+namespace LabTestAppointments.Data.Interfaces.Repositories
+{
+    public interface IBioanalystRepository : IGenericRepository<Bioanalyst>
+    {
+    }
+}
