d823ca38b9670df8794bf5ccb56a9d682484c735
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-06-27 21:37:03 -0400

    Adding IHealthInsuranceRepository interface.

---
+++ LabTestAppointments.Data/Interfaces/IHealthInsuranceRepository.cs
@@ -0,0 +1,11 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories.Generic;
+
+namespace LabTestAppointments.Data.Interfaces.Repositories
+{
+    public interface IHealthInsuranceRepository : IGenericRepository<HealthInsurance>
+    {
+        Task<List<HealthInsurance>> GetAllWithIncludeAsync(List<string> properties);
+        Task<HealthInsurance> GetByIdWithIncludeAsync(int id, List<string> properties);
+    }
+}
