f63d7cce3a7e0e771d727073e2d8b8f55d7badbc
Alexis Ramirez <alexisramirez0122@hotmail.com>
2024-06-28 00:13:09 -0400

    Creating index view

---
+++ LabTestAppointments.Web/Controllers/LabTestController.cs
@@ -0,0 +1,19 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTestAppointments.Web.Controllers
+{
+    public class LabTestController : Controller
+    {
+        private readonly ILabTestRepository _labTestRepository;
+        public LabTestController(ILabTestRepository labTestRepository)
+        {
+            _labTestRepository = labTestRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _labTestRepository.GetAllAsync());
+        }
+    }
+}
