6c069c51f72c4fd29ffd152da36bcf4b91900cb4
Pedro Antonio <20222033@itla.edu.do>
2024-06-28 01:20:25 -0400

    Creating insuranceProviderIndex View.

---
+++ LabTestAppointments.Web/Controllers/InsuranceProviderController.cs
@@ -0,0 +1,19 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTestAppointments.Web.Controllers
+{
+    public class InsuranceProviderController : Controller
+    {
+        private readonly IInsuranceProviderRepository _insuranceProviderRepository;
+        public InsuranceProviderController(IInsuranceProviderRepository insuranceProviderRepository)
+        {
+            _insuranceProviderRepository = insuranceProviderRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _insuranceProviderRepository.GetAllAsync());
+        }
+    }
+}
