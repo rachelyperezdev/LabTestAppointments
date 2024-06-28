bfbd9d6f0f73f5ce70648859e6bebf9d7a33a2bd
RachelyPerez <68482998+rachelyperez31@users.noreply.github.com>
2024-06-28 01:54:25 -0400

    Creating HealthInsurance index view.

---
+++ LabTestAppointments.Web/Controllers/HealthInsuranceController.cs
@@ -0,0 +1,21 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTestAppointments.Web.Controllers
+{
+    public class HealthInsuranceController : Controller
+    {
+        private readonly IHealthInsuranceRepository _healthInsuranceRepository;
+        private readonly IInsuranceProviderRepository _insuranceProviderRepository;
+        public HealthInsuranceController(IHealthInsuranceRepository healthInsuranceRepository, IInsuranceProviderRepository insuranceProviderRepository)
+        {
+            _healthInsuranceRepository = healthInsuranceRepository;
+            _insuranceProviderRepository = insuranceProviderRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _healthInsuranceRepository.GetAllWithIncludeAsync(new List<string>() { "InsuranceProvider" }));
+        }
+    }
+}
