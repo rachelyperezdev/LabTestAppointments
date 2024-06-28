59ce8ffe2bb37109f7ffa380253cfe62c89841b0
HeyHector5 <hectorgmedina05@gmail.com>
2024-06-28 00:34:14 -0400

    Creating Index view for Bioanalyst.

---
+++ LabTestAppointments.Web/Controllers/BioanalystController.cs
@@ -0,0 +1,19 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTestAppointments.Web.Controllers
+{
+    public class BioanalystController : Controller
+    {
+        private readonly IBioanalystRepository _bioanalystRepository;
+        public BioanalystController(IBioanalystRepository bioanalystRepository)
+        {
+            _bioanalystRepository = bioanalystRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _bioanalystRepository.GetAllAsync());
+        }
+    }
+}
