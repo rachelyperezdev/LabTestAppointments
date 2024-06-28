a267dc3f9135d316e811b62508a1555901ab0b5d
Alexander Hilario <hilariocastilloalexander@gmail.com>
2024-06-27 23:24:55 -0400

    Creating MedicalImage Index view

---
+++ LabTestAppointments.Web/Controllers/MedicalImageController.cs
@@ -0,0 +1,21 @@
+﻿using LabTestAppointments.Data.Entities;
+using LabTestAppointments.Data.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabTestAppointments.Web.Controllers
+{
+    public class MedicalImageController : Controller
+    {
+        private readonly IMedicalImageRepository _medicalImageRepository;
+
+        public MedicalImageController(IMedicalImageRepository medicalImageRepository)
+        {
+            _medicalImageRepository = medicalImageRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _medicalImageRepository.GetAllAsync());
+        }
+    }
+}
