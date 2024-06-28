265442cc6ea6c5e24a67cf44e4ff473a95a3480f
Alexis Ramirez <alexisramirez0122@hotmail.com>
2024-06-27 21:54:08 -0400

    Adding LabTest interfaces

---
+++ LabTestAppointments.Data/Exceptions/LabTest/LabTestNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace LabTestAppointments.Data.Exceptions.LabTest
+{
+    public class LabTestNotFoundException : Exception
+    {
+        public LabTestNotFoundException(string message) : base(message)
+        {
+
+        }
+    }
+}
